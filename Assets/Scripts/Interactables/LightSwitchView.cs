3d74f7219aca08de11622296f3877e433512b277
rangamach <117797904+rangamach@users.noreply.github.com>
2025-05-08 11:48:34 +0530

    changed delegate to action

--- Assets/Scripts/Interactables/LightSwitchView.cs
+++ Assets/Scripts/Interactables/LightSwitchView.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 using static LightSwitchView;
@@ -6,16 +7,15 @@ public class LightSwitchView : MonoBehaviour, IInteractable
 {
     [SerializeField] private List<Light> lightsources = new List<Light>();
     private SwitchState currentState;
-    public delegate void LightSwitchDelegate();
-    public static event LightSwitchDelegate lightToggled;
+    public static Action lightToggledAction;
 
-    private void OnEnable() => lightToggled += onLightSwitch;
+    private void OnEnable() => lightToggledAction += onLightSwitch;
 
-    private void OnDisable() => lightToggled -= onLightSwitch;
+    private void OnDisable() => lightToggledAction -= onLightSwitch;
 
     private void Start() => currentState = SwitchState.Off;
 
-    public void Interact() => lightToggled?.Invoke();
+    public void Interact() => lightToggledAction?.Invoke();
 
     private void toggleLights()
     {
