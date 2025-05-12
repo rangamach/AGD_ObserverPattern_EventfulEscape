db5f168b2dce685bcb42e516c44884761f38fc16
rangamach <117797904+rangamach@users.noreply.github.com>
2025-05-12 10:33:33 +0530

    Checking for the appropriate keys required to trigger the event

--- Assets/Scripts/Events/LightsOffByGhostEvent.cs
+++ Assets/Scripts/Events/LightsOffByGhostEvent.cs
@@ -2,5 +2,16 @@ using UnityEngine;
 
 public class LightsOffByGhostEvent : MonoBehaviour
 {
+    [SerializeField] private int requred_keys;
+    [SerializeField] private SoundType sound_effect;
 
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.GetComponent<PlayerView>() != null && requred_keys == GameService.Instance.GetPlayerController().KeysEquipped)
+        {
+            EventService.Instance.OnLightsOffByGhostEvent.InvokeEvent();
+            GameService.Instance.GetSoundView().PlaySoundEffects(sound_effect);
+            this.enabled = false;
+        }
+    }
 }
